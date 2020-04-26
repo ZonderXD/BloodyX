caf8c72c28c96f808a7aec97f6c70847071b7e0a
ZonderXD <62016427+ZonderXD@users.noreply.github.com>
2020-04-26 15:06:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -58,10 +58,10 @@ async def suggest( ctx , * , agr ):
 @commands.check(is_owner)
 async def opros(ctx, *, arg):
     await ctx.message.delete()
-    embed = discord.Embed(title=f"{ctx.guild.name}", description="Опрос:`**", color = 0x00ffff)
+    embed = discord.Embed(title=f"ОПРОС:`**", color = 0x00ffff)
     embed.add_field(name=f'**Вопрос:**', value=f"**{arg}**\n", inline=False)  # Создает строку
     embed.add_field(name=f'**Решение:**', value="**-=-=- Да - ❤ -=-=-\n -=-=- Нет - 💔 -=-=-**\n\n", inline=False)  # Создает строку
-    embed.add_field(name=f'**Решение:**', value="**Выбор за Вами!**", inline=False)  # Создает строку
+    embed.add_field(name=f'**Инфо:**', value="**Выбор за Вами!**", inline=False)  # Создает строку
     await ctx.send(embed=embed)
     await ctx.message.add_reaction('❤')
     await ctx.message.add_reaction('💔')
