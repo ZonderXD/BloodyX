d6ce5bb23db918f190d01c53223e98f763bdebbd
ZonderXD <62016427+ZonderXD@users.noreply.github.com>
2020-04-27 19:49:08 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -82,9 +82,9 @@ async def on_message(message):
     conn.commit()
 
 @bot.command()
-async def level(ctx):
+async def rang(ctx):
     for i in cursor.execute(f"SELECT lvl, xp FROM users WHERE id = {ctx.author.id}"):
-        await ctx.send(embed = discord.Embed(description = f'**Твой уровень: `{i[0]}`, а вот твой опыт: `{i[1]}`', color=0x00ffff))
+        await ctx.send(embed = discord.Embed(description = f'**Твой уровень: `{i[0]}`, а вот твой опыт: `{i[1]}`**', color=0x00ffff))
 
 @bot.command()
 @commands.check(is_owner)
