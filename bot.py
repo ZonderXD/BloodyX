002536ce61e1e1dcea6d11a96d5af0d9ee990fd0
ZonderXD <62016427+ZonderXD@users.noreply.github.com>
2020-04-27 19:29:39 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -73,7 +73,7 @@ async def on_message(message):
             new_xp = i[1] + len(message.content)
 
     if new_xp >= lvl * 100 + 1000:
-	await message.channel.send(f'{message.author.mention} кросс, теперь у тебя {lvl + 1} см!')
+        await message.channel.send(f'{message.author.mention} кросс, теперь у тебя {lvl + 1} см!')
             lvl += 1
             new_xp = 0
         
