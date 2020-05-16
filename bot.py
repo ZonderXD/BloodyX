9df6e4811eda67fb08fe8789e82191a699f48b45
ZonderXD <62016427+ZonderXD@users.noreply.github.com>
2020-05-16 19:27:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -27,7 +27,7 @@ cursor = conn.cursor()
 @bot.event
 async def on_ready():
     print(f'          [Bloody X]')
-    await bot.change_presence(status = discord.Status.idle, activity = discord.Game('Bloody X'))
+    await bot.change_presence(status = discord.Status.idle, activity = discord.Game('⊱ Prefix: . ⊰'))
     print(f"[Bloody X] Bot successfully launched!;")
     print(f"[Bloody X] Name: [{bot.user}];")
     print(f'[Bloody X] ID: [{bot.user.id}]')
