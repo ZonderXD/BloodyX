f577fe169d50c0435f5464eb3fd5503b9771e8c3
ZonderXD <62016427+ZonderXD@users.noreply.github.com>
2020-05-16 14:32:36 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -72,25 +72,12 @@ async def on_message(msg):
                     print(f"⊱ {author.name}, произнёс слово [{msg.content}] ⊰")
     
         mat.close()
-
-    if len(message.content) > 5:
-        for i in cursor.execute(f"SELECT lvl, xp FROM users where id = {message.author.id}"):
-            lvl = i[0]
-            new_xp = i[1] + len(message.content)
-
-    if new_xp >= lvl * 100 + 1000:
-        await message.channel.send(f'{message.author.mention} кросс, теперь у тебя {lvl + 1} см!')
-        lvl += 1
-        new_xp = 0
-        
-    cursor.execute(f'UPDATE users SET lvl = {lvl}, xp = {new_xp} WHERE id = {message.author.id}')
-    conn.commit()
     
-    cursor.execute(f"SELECT * FROM users WHERE id = {message.author.id}")
+    cursor.execute(f"SELECT * FROM users WHERE id = {msg.author.id}")
     res = cursor.fetchall()
 
     if not res:
-        cursor.execute(f"INSERT INTO users (id, nickname, money, lvl, xp) VALUES ({message.author.id}, '{message.author.name}', 0, 0, 0)")
+        cursor.execute(f"INSERT INTO users (id, nickname, money, lvl, xp) VALUES ({msg.author.id}, '{msg.author.name}', 0, 0, 0)")
         conn.commit()
 
 @bot.command()
