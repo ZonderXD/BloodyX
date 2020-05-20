3f4d9297ba538e76e464bd352bded21c18a88372
ZonderXD <62016427+ZonderXD@users.noreply.github.com>
2020-05-20 19:42:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -85,7 +85,7 @@ async def on_voice_state_update(member,before,after):
         for guild in bot.guilds:
             if guild.id == 696322642747064380:
                 mainCategory = discord.utils.get(guild.categories, id=712629625049579561)
-                channel2 = await guild.create_voice_channel(name=f":sunrise_over_mountains: | {member.display_name}",category=mainCategory, limit=1)
+                channel2 = await guild.create_voice_channel(name=f"🌄 | {member.display_name}",category=mainCategory, user_limit=1)
                 await member.move_to(channel2)
                 await channel2.set_permissions(member,manage_channels=True)
                 def check(a,b,c):
