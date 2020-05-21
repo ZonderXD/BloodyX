9d5d9c6e4b2c135eb720dac4c5b569018ee5ce82
ZonderXD <62016427+ZonderXD@users.noreply.github.com>
2020-05-21 11:25:41 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -221,15 +221,11 @@ async def giveaway( ctx, seconds: int, *, text ):
 async def neko(ctx):
     number = random.randint(1,2)
     if (number == 1): 
-        embed = discord.Embed(
-            description = f"{ctx.author.mention} вот тебе гиф!", 
-            colour = 0xff0000)
-            embed.set_image(url=nekos.img('neko'))
+        embed = discord.Embed(description = f"{ctx.author.mention} вот тебе гиф!", colour = 0xff0000)
+        embed.set_image(url=nekos.img('neko'))
     if (number == 2):
-        embed = discord.Embed(
-            description = f"{ctx.author.mention} дружески обнял(а) {member.mention}",
-            colour = 0xff0000)
-            embed.set_image(url=nekos.img('fox_girl'))
+        embed = discord.Embed(description = f"{ctx.author.mention} дружески обнял(а) {member.mention}", colour = 0xff0000)
+        embed.set_image(url=nekos.img('fox_girl'))
     await ctx.send(embed = embed)
 
 @bot.command()
