748f14b5f8f228458c6c0c4cac0fa818cad94af8
ZonderXD <62016427+ZonderXD@users.noreply.github.com>
2020-06-02 22:14:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -62,7 +62,9 @@ def random_meme():
 @bot.check_once
 def blacklist(ctx):
     blacklist_member = ['475239960778244097', '716724192065749075']
- 
+    if ctx.message.author.id in blacklist_member:
+        pass
+
     return ctx.message.author.id not in blacklist_member
 
 @bot.command()
