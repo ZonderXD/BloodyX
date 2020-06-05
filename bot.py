8dadb6d1b0ea247c61bc0e84d8853cc81bbb498d
ZonderXD <62016427+ZonderXD@users.noreply.github.com>
2020-06-05 23:33:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -89,7 +89,7 @@ async def giveaway( ctx, seconds: int, *, text ):
     message = await ctx.send(embed = discord.Embed(
         description = f"**Разыгрывается : `{text}`\nЗавершится через: `{time_end}` \n\nОрганизатор: {author.mention} \nДля участия нажмите на реакцию ниже.**",
         colour = 0x75218f).set_footer(
-        text = '𝙵𝚛𝚒𝚍ツ#9691 © | Все права защищены',
+        text = '! 𝐑 𝐘 𝐍 Ξ 𝐗#9691 © | Все права защищены',
         icon_url = ctx.message.author.avatar_url))
     await message.add_reaction("🎉")
     while seconds > -1:
@@ -97,7 +97,7 @@ async def giveaway( ctx, seconds: int, *, text ):
         text_message = discord.Embed(
             description = f"**Разыгрывается: `{text}`\nЗавершится через: `{time_end}` \n\nОрганизатор: {author.mention} \nДля участия нажмите на реакцию ниже.**",
             colour = 0x75218f).set_footer(
-            text = '𝙵𝚛𝚒𝚍ツ#9691 © | Все права защищены',
+            text = '! 𝐑 𝐘 𝐍 Ξ 𝐗#9691 © | Все права защищены',
             icon_url = ctx.message.author.avatar_url)
         await message.edit(embed = text_message)
         await asyncio.sleep(1)
@@ -120,7 +120,7 @@ async def giveaway( ctx, seconds: int, *, text ):
             win = discord.Embed(
                 description = f'**В этом розыгрыше нет победителя!**',
                 colour = 0x75218f).set_footer(
-                text = '𝙵𝚛𝚒𝚍ツ#9691 © | Все права защищены',
+                text = '! 𝐑 𝐘 𝐍 Ξ 𝐗#9691 © | Все права защищены',
                 icon_url = ctx.message.author.avatar_url)
         elif str(user_win.id) == str(bot.user.id):
             winners()
@@ -128,7 +128,7 @@ async def giveaway( ctx, seconds: int, *, text ):
             win = discord.Embed(
                 description = f'**Победитель розыгрыша: {user_win.mention}!\nНапишите организатору {author.mention}, чтобы получить награду.**',
                 colour = 0x75218f).set_footer(
-                text = '𝙵𝚛𝚒𝚍ツ#9691 © | Все права защищены',
+                text = '! 𝐑 𝐘 𝐍 Ξ 𝐗#9691 © | Все права защищены',
                 icon_url = ctx.message.author.avatar_url)
 
     winners()
@@ -136,7 +136,7 @@ async def giveaway( ctx, seconds: int, *, text ):
     await message.edit(embed = win)
     await author.send(embed = discord.Embed(description = f'**Ваш розыгрыш закончился.**',
                                             colour = 0x75218f).set_footer(
-        text = '𝙵𝚛𝚒𝚍ツ#9691 © | Все права защищены',
+        text = '! 𝐑 𝐘 𝐍 Ξ 𝐗#9691 © | Все права защищены',
         icon_url = ctx.message.author.avatar_url))
 
 @bot.command( pass_context = True, aliases = [ "Предложить", "предложить", "предложка", "Предложка", "Suggest" ])
@@ -295,14 +295,14 @@ async def on_member_remove( member ):
 @bot.command(aliases=['bot'])
 async def botinfo(ctx):
     embed = discord.Embed(title=f"{ctx.guild.name}", description="Информация о боте **𝐖𝐨𝐨𝐟 𝐗#7002**.\n Бот был написан специально для проекта **`Woof X`**,\n Подробнее о командах: **`.help`**", color = 0x00ffff)
-    embed.add_field(name=f'**Меня создал:**', value="`𝙵𝚛𝚒𝚍ツ#9691`(<@716724192065749075>)", inline=False)  # Создает строку
+    embed.add_field(name=f'**Меня создал:**', value="`! 𝐑 𝐘 𝐍 Ξ 𝐗#9691`(<@716724192065749075>)", inline=False)  # Создает строку
     embed.add_field(name=f'**Помощь в создании:**', value="`Satana★#2362`(<@342317507991961602>)", inline=False)  # Создает строку
     embed.add_field(name=f'**Лицензия:**', value="LD-v7", inline=False)  # Создает строку
     embed.add_field(name=f'**Я написан на:**', value="Discord.py", inline=False)  # Создает строку
     embed.add_field(name=f'**Версия:**', value="V.3.0.1", inline=False)  # Создает строку
     embed.add_field(name=f'**Патч:**', value="10", inline=False)  # Создает строку
     embed.set_thumbnail( url = bot.user.avatar_url)
-    embed.set_footer(text=f"𝙵𝚛𝚒𝚍ツ#9691 © | Все права защищены", icon_url='https://cdn.discordapp.com/avatars/716724192065749075/4bf146f16ef794b7f5512438ca0970a7.webp?size=1024') # создаение футера
+    embed.set_footer(text=f"! 𝐑 𝐘 𝐍 Ξ 𝐗#9691 © | Все права защищены", icon_url='https://cdn.discordapp.com/avatars/716724192065749075/9d9ea47a88a73c325a4317f0b4e06c34.webp?size=1024') # создаение футера
     await ctx.send(embed=embed)
 
 @bot.command()
