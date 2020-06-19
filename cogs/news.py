878d655fbaf5b9298149dde2715b0b0acf54dd63
ZonderXD <62016427+ZonderXD@users.noreply.github.com>
2020-06-19 19:15:24 +0300

    Update news.py

--- cogs/news.py
+++ cogs/news.py
@@ -1,3 +1,30 @@
+import discord
+import asyncio
+import datetime
+import random as r
+import random
+import io
+import os
+import wikipedia
+import nekos
+import sqlite3
+import json
+import requests
+import time
+import sys
+import traceback
+import youtube_dl
+from mod import *
+from discord.ext import commands
+from discord.utils import get
+from yandex_music import Client
+from Cybernator import Paginator
+
+class user(commands.Cog):
+
+    def init(self, bot):
+        self.bot = bot
+
     @commands.group(description = 'Создать пост от имени бота', hidden = True)
     @commands.is_owner()
     async def post(self, ctx):
