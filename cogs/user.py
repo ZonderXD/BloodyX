88f28aab49dec7f89bfd7f2197a5380f42578e4a
ZonderXD <62016427+ZonderXD@users.noreply.github.com>
2020-06-19 16:57:53 +0300

    Add files via upload

---
+++ cogs/user.py
@@ -0,0 +1,37 @@
+import discord
+import asyncio
+import datetime
+import random as r
+import random
+import io
+import os
+import wikipedia
+import nekos
+import sqlite3
+import json
+import requests
+import time
+import sys
+import traceback
+import youtube_dl
+from mod import *
+from discord.ext import commands
+from discord.utils import get
+from yandex_music import Client
+from Cybernator import Paginator
+
+class user(commands.Cog):
+    
+    def __init__(self, client):
+        self.bot = bot
+
+@commands.command()
+    async def example(self,ctx):
+        await ctx.send(“work”)
+
+@commands.Cog.listener()
+    async def on_ready (self):
+        print(“ready”)
+
+def setup(bot):
+    client.add_cog(user(bot))
